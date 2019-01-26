e922fad5103e7900e4fb9bafa85358be82153a90
Laura Alejandra Chaparro Gutiérrez <lauralejandracg@gmail.com>
2019-01-25 21:02:06 -0500

    Fix message validation function

--- app/assets/javascripts/message_validation.js
+++ app/assets/javascripts/message_validation.js
@@ -3,33 +3,23 @@ function validate_message(message, encrypted_hash){
 
     let url = window.location.protocol + "//" + window.location.host + "/public_key";
 
-    async function getPublicKey() {
-        try{
-            let res = await fetch(url);
-            return res.json();
-        }
-        catch (e) {
-            console.warn("Not possible to retrieve public key due to: ", e.toString());
-            return null;
-        }
-    }
+    return fetch(url)
+        .then(response => response.json())
+        .then(function (pk) {
+           if(pk.length == 2){
+               let e = pk[0];
+               let n = pk[1];
 
-    (async function () {
-        let ans = await getPublicKey();
-        if(ans != null && ans.length == 2){
-            console.log("Hash for message ", message, ": ", md5(message));
+               let hash = md5(message);
+               let arr_hash = split_with_base(hash, n, BLOCK_SIZE);
+               let enc_hash_pk_server_arr = rsa(arr_hash, pk);
+               let enc_hash_pk_server = join_with_base(enc_hash_pk_server_arr, n);
 
-            let hash = md5(message);
-            let n = ans[1];
-            let arr_hash = split_with_base(message, n, BLOCK_SIZE);
-            let enc_hash_pk_server_arr = rsa(arr_hash, ans);
-            let enc_hash_pk_server = join_with_base(enc_hash_pk_server_arr, n);
-
-            console.log("Validation: " , new String(enc_hash_pk_server).trim().valueOf() == new String(encrypted_hash).trim().valueOf());
-
-            // TODO: fix return from async function
-            return Boolean(new String(enc_hash_pk_server).trim().valueOf() == new String(encrypted_hash).trim().valueOf());
-        }
-        return false;
-    })();
+               return enc_hash_pk_server == encrypted_hash;
+           }
+           return false;
+        })
+        .catch(function (error) {
+            console.log("Error while retrieving public key from server: " + error);
+        });
 }
