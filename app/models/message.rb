46ac1b73e50ef6d40246e3a1d0a9657b272f2ee9
fffeelipe <lfepiar@unal.edu.co>
2019-01-26 17:13:40 -0500

    Backend now returns signature with MD5 =D

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
+include MathUtils
+
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
@@ -6,5 +8,15 @@ class Message < ActiveRecord::Base
   def message_time
     created_at.strftime("%m/%d/%y a las %l:%M %p")
   end
+
+  def signature
+    md5 = Digest::MD5.new
+    md5 << body 
+    s = md5.hexdigest.to_i
+    s = power_mod(s, $rsa_keys.public_key[0], $rsa_keys.public_key[1])
+
+    
+  end
+
  end
  
