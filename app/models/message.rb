b701f7d8bac20af1bf78512916763f98af20ba8e
fffeelipe <lfepiar@unal.edu.co>
2019-01-27 10:55:14 -0500

    Sorry

--- app/models/message.rb
+++ app/models/message.rb
@@ -13,9 +13,19 @@ class Message < ActiveRecord::Base
     md5 = Digest::MD5.new
     md5 << body 
     s = md5.hexdigest
-    s = power_mod(s.to_i(16), $rsa_keys.public_key[0], $rsa_keys.public_key[1])
-
+    s = power_mod(s.to_i(16), $rsa_keys.private_key[0], $rsa_keys.private_key[1])
+  end
+  
+  def checkSignature
+    md5 = Digest::MD5.new
+    md5 << body 
+    s = md5.hexdigest.to_i(16)
     
+    #puts "hash = "
+    #puts s % $rsa_keys.public_key[1]
+    #puts "decrypt = "
+    #puts power_mod(signature,$rsa_keys.public_key[0], $rsa_keys.public_key[1])
+    puts (power_mod(signature,$rsa_keys.public_key[0], $rsa_keys.public_key[1]) == s % $rsa_keys.public_key[1])
   end
 
  end
